f280ce47898ee5085d79b6813c42de7d64c3baa5
Jono Kumarich <90363070+JonoKumarichWWNZ@users.noreply.github.com>
2022-09-09 08:20:31 +1200

    Batch prediction save to csv

--- main.py
+++ main.py
@@ -3,14 +3,11 @@ import joblib
 import tensorflow as tf
 from typing import Dict
 import os
+import csv
 
 WIDTH, HEIGHT = (256, 256)
 
 app = typer.Typer(no_args_is_help=True)
-
-print("Loading model from pickle file...")
-model = joblib.load("model.pkl")
-print("Model loading complete!")
 
 
 @app.command(short_help="Predicts probability of rat detected in image at path.")
@@ -34,15 +31,27 @@ File at path: {image_path} \nRat presence prediction: {prediction:2f}
 
 
 @app.command(short_help="Predicts probabilities for all images in a folder")
-def batch_prediction(folder_path: str) -> Dict[str, float]:
+def batch_prediction(folder_path: str, save_results: bool = False) -> Dict[str, float]:
     preds = {}
+
     for file in os.listdir(folder_path):
         pred = predict_image(f"{folder_path}/{file}")
         preds[file] = pred
-    # TODO: Output these predictions to a csv
+
+    if save_results:
+        with open('./outputs/batch_predictions.csv', 'w') as f:
+            w = csv.DictWriter(f, ['filename', 'prediction'])
+            w.writeheader()
+            rows = [{"filename": key,
+                     "prediction": preds[key]
+                     } for key in preds.keys()]
+            w.writerows(rows)
+
     return preds
 
 
 if __name__ == "__main__":
+    print("Loading model from pickle file...")
+    model = joblib.load("model.pkl")
+    print("Model loading complete!")
     app()
-    # predict_image("./test_images/empty_0001.JPG")
